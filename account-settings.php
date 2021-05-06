228d882d52fde95e754e3b13a2b437389ff3763c
alysxandra <71811550+alysxandra@users.noreply.github.com>
2021-05-06 22:48:33 +0400

    um.. edit profile... work in progress... Aha...

---
+++ account-settings.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" type="text/css" href="css/user-info.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    
+
+
+</head>
+<body>
+
+    <div class="navbg"> 
+    <?php include 'includes/headers-edit.php'; ?> 
+    </div>
+
+    <div class="container-1">
+        
+        <!-- BACKGROUND -->
+        <div class="background-container"></div>
+    
+
+        <!-- PROFILE -->
+        <div class="profile-container">
+            <div class="profile-pic"></div>
+        </div>
+
+        <h1>Edit Profile</h1>
+
+        <a class="u-info" href="user-info.php">User info</a>
+        <h2>Account settings</h2>
+
+        <p>Email</p>
+        <p class="two">Password</p>
+
+    </div>
+
+</body>
+</html>
---
+++ user-info.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" type="text/css" href="css/user-info.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    
+
+
+</head>
+<body>
+
+    <div class="navbg"> 
+    <?php include 'includes/headers-edit.php'; ?> 
+    </div>
+
+    <div class="container-1">
+
+        <!-- BACKGROUND -->
+        <div class="background-container"></div>
+    
+
+        <!-- PROFILE -->
+        <div class="profile-container">
+            <div class="profile-pic"></div>
+        </div>
+
+        <h1>Edit Profile</h1>
+
+        <h2>User info</h2>
+
+        <a class="acc-s" href="account-settings.php">Account settings</a>
+
+        <p>First Nameㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤㅤㅤㅤ Last Name</p>
+        <p class="two">Location</p>
+        <p class="three">Phone Number</p>
+
+    </div>
+
+</body>
+</html>
