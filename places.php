298daef5806fb1705944ba1eb2542c0549b86e59
cctrolley <80287059+cctrolley@users.noreply.github.com>
2021-05-16 04:01:22 +0400

    after almost 2 weeks of sorrow, i did it
    kulang nlng slider on the 1st gallery thing and the event names below the calendar and this is G for reproduction it just so happens 4am na and di na kaya ng kaluluwa q thank u for reading thru this godbless

---
+++ places.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PLACE HOLDER FOR PLACES</title>
+        
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/places.css">
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+
+
+    
+
+
+</head>
+<body>
+    <div class="navbg"> 
+    <?php include 'includes/headers.php'; ?> 
+    </div>
+
+    <div class="content-wrapper">
+    <!-- TITLE -->
+        <div class="title-container">
+            <div class="picture"></div>
+            <h1> PLACEHOLDER </h1>
+            <p class="location-title"> PLACEHOLDER </p>
+            <div class="line1"></div>
+        </div>
+    <!-- CATEGORIES -->
+        <div class="categories">
+                <div class="box"><a href="#">HISTORY</a></div>
+                <div class="box"><a href="#">WHAT TO DO</a></div>
+                <div class="box"><a href="#">TIMINGS</a></div>
+                <div class="box"><a href="#">EVENTS</a></div>
+                <div class="box"><a href="#">VIRTUAL TOUR</a></div>
+                <div class="box"><a href="#">REVIEWS</a></div>
+                <div class="box"><a href="#">MEDIA</a></div>
+        </div>
+
+
+    <!-- BACKGROUND 1 -->
+
+            <div class="rectangle"></div>
+
+    <!-- INFORMATION -->
+        <div class="layer1">
+        <div class="info-hist-box">
+        <div class="info-container">
+            <h2 class="info-title"> Information </h2>
+            <p class="info-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst quisque 
+                sagittis. Lorem donec massa sapien faucibus et. Iaculis urna id volutpat 
+                lacus laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. 
+                At risus viverra adipiscing at in. Tempor nec feugiat nisl pretium. Iaculis urna id volutpat 
+                lacus laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. 
+                At risus viverra adipiscing at in. Tempor nec feugiat nisl pretium. Iaculis urna id volutpat 
+                lacus laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. 
+                At risus viverra adipiscing at in. Tempor nec feugiat nisl pretium. Tempor nec feugiat nisl pretium.
+                Tempor nec feugiat nisl pretium. </p>
+        </div>
+        <div class="hist-container">
+            <h2 class="history-title"> History </h2>
+            <p class="history-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst quisque 
+                sagittis. Lorem donec massa sapien faucibus et. Iaculis urna id volutpat 
+                lacus laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. 
+                At risus viverra adipiscing at in. Tempor nec feugiat nisl pretium. <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst quisque 
+                sagittis. Lorem donec massa sapien faucibus et. Iaculis urna id volutpat 
+                lacus laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. 
+                At risus viverra adipiscing at in. Tempor nec feugiat nisl pretium. <br><br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst quisque 
+                sagittis. Lorem donec massa sapien faucibus et. Iaculis urna id volutpat 
+                lacus laoreet. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. 
+                At risus viverra adipiscing at in. Tempor nec feugiat nisl pretium. <br><br>
+                </p>
+        </div>
+        <div class="left-images">
+            <img src="images/PLACEHOLDER.png" class="img-set"/>
+            <img src="images/PLACEHOLDER.png" class="img-set"/>
+            <img src="images/PLACEHOLDER.png" class="img-set"/>
+        </div>
+        </div>
+        </div>
+
+
+
+        <!-- WHAT TO DO -->
+        <div class="wtd-container">
+            <div class="scroller-area">
+            
+            <div class="contents-box">
+                <img src="images/PLACEHOLDER.png" class="contents-img"/>
+                <div class="contents-container">
+                <h2 class="contents-title"> Placeholder </h2>
+                <p class="contents-desc">Justo nec ultrices dui sapien eget. Sagittis purus sit amet eu volutpat consequat.
+                 Feugiat in ante metus dictum at tempor commodo. </p>
+                 </div>
+            </div> 
+            
+            <div class="contents-box">
+                <img src="images/PLACEHOLDER.png" class="contents-img"/>
+                <div class="contents-container">
+                <h2 class="contents-title"> Placeholder </h2>
+                <p class="contents-desc">Justo nec ultrices dui sapien eget. Sagittis purus sit amet eu volutpat consequat.
+                 Feugiat in ante metus dictum at tempor commodo. </p>
+                 </div>
+            </div> 
+            
+
+            <div class="contents-box">
+                <img src="images/PLACEHOLDER.png" class="contents-img"/>
+                <div class="contents-container">
+                <h2 class="contents-title"> Placeholder </h2>
+                <p class="contents-desc">Justo nec ultrices dui sapien eget. Sagittis purus sit amet eu volutpat consequat.
+                 Feugiat in ante metus dictum at tempor commodo. </p>
+                 </div>
+            </div>
+
+            <div class="contents-box">
+                <img src="images/PLACEHOLDER.png" class="contents-img"/>
+                <div class="contents-container">
+                <h2 class="contents-title"> Placeholder </h2>
+                <p class="contents-desc">Justo nec ultrices dui sapien eget. Sagittis purus sit amet eu volutpat consequat.
+                 Feugiat in ante metus dictum at tempor commodo. </p>
+                 </div>
+            </div>
+
+            </div>
+        </div>
+        <!-- -->
+
+        <!-- TIMINGS -->
+        <div class="timing-container">
+            <h2 class="timing-title">TIMINGS</h2>
+            <div class="timing-week">
+                <div class="days"><a href="#">SUNDAY</a></div>
+                <div class="days"><a href="#">MONDAY</a></div>
+                <div class="days"><a href="#">TUESDAY</a></div>
+                <div class="days"><a href="#">WEDNESDAY</a></div>
+                <div class="days"><a href="#">THURSDAY</a></div>
+                <div class="days"><a href="#">FRIDAY</a></div>
+                <div class="days"><a href="#">SATURDAY</a></div>
+            </div>
+
+            <div class="t-container">
+            <div class="timing-info">
+                <div class="time">
+                   <p id="t-text"> PLACE-HOLD </p>
+                </div>
+                <div class="t-info">
+                    <p id="t-text"> PLACEHOLDER PLACEHOLDER UHUH PLACING THE HOLDER </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- -->
+
+
+        <!-- calendar -->
+       
+        <h2 class="calendar-title">EVENTS</h2>
+        <div class="calendar-bg"> 
+       
+        <div class="main">
+        <div class="sideb">
+        <div class="header"><i class="fa fa-angle-left" aria-hidden="true">
+        </i><span><span class="month"> </span><span class="year"></span></span><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+        <div class="calender">
+        <table>
+        <thead>
+          <tr class="weedays">
+            <th data-weekday="sun" data-column="0">Sun</th>
+            <th data-weekday="mon" data-column="1">Mon</th>
+            <th data-weekday="tue" data-column="2">Tue</th>
+            <th data-weekday="wed" data-column="3">Wed</th>
+            <th data-weekday="thu" data-column="4">Thu</th>
+            <th data-weekday="fri" data-column="5">Fri</th>
+            <th data-weekday="sat" data-column="6">Sat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="days" data-row="0">
+            <td data-column="0"></td>
+            <td data-column="1"></td>
+            <td data-column="2"></td>
+            <td data-column="3"></td>
+            <td data-column="4"></td>
+            <td data-column="5"></td>
+            <td data-column="6"></td>
+          </tr>
+          <tr class="days" data-row="1">
+            <td data-column="0"></td>
+            <td data-column="1"></td>
+            <td data-column="2"></td>
+            <td data-column="3"></td>
+            <td data-column="4"></td>
+            <td data-column="5"></td>
+            <td data-column="6"></td>
+          </tr>
+          <tr class="days" data-row="2">
+            <td data-column="0"></td>
+            <td data-column="1"></td>
+            <td data-column="2"></td>
+            <td data-column="3"></td>
+            <td data-column="4"></td>
+            <td data-column="5"></td>
+            <td data-column="6"></td>
+          </tr>
+          <tr class="days" data-row="3">
+            <td data-column="0"></td>
+            <td data-column="1"></td>
+            <td data-column="2"></td>
+            <td data-column="3"></td>
+            <td data-column="4"></td>
+            <td data-column="5"></td>
+            <td data-column="6"></td>
+          </tr>
+          <tr class="days" data-row="4">
+            <td data-column="0"></td>
+            <td data-column="1"></td>
+            <td data-column="2"></td>
+            <td data-column="3"></td>
+            <td data-column="4"></td>
+            <td data-column="5"></td>
+            <td data-column="6"></td>
+          </tr>
+          <tr class="days" data-row="5">
+            <td data-column="0"></td>
+            <td data-column="1"></td>
+            <td data-column="2"></td>
+            <td data-column="3"></td>
+            <td data-column="4"></td>
+            <td data-column="5"></td>
+            <td data-column="6"></td>
+          </tr>
+        </tbody>
+        </table>
+        </div>
+        <div class="showDate"><i class="fa fa-angle-double-right" aria-hidden="true"></i></div>
+        </div>
+        <div class="right-wrapper">
+        <div class="header"><span></span></div><span class="day"></span><span class="month"></span>
+        </div>
+        </div>
+
+        </div>
+
+    <!-- -->
+
+    <!-- VIRTUAL TOUR -->
+    <h2 class="vt-title">VIRTUAL TOUR</h2>
+    <!-- to be replaced -->
+    <iframe width="100%" height="1000px" src="https://poly.google.com/view/ewUGbxgQ_bL/embed?chrome=min" 
+        frameborder="0" style="border:none;" allowvr="yes" 
+        allow="vr; xr; accelerometer; magnetometer; gyroscope; autoplay;" 
+        allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel="" >
+    </iframe>	
+    <!-- -->
+    
+    <!-- COMMENTS -->
+    <h2 class="review-title">REVIEWS</h2>
+  
+    <div class="review-container" id="review-container">
+        
+        <div class="user-review">
+       
+            <div class="main-review review">
+            <img src="images/PLACEHOLDER.png" class="pfp"/>
+                <div class="r-box main-box">
+                <div class="user">Avery Angel <div class="dot"></div>
+                <span class="date">Apr 01 2012</span></div>
+                <div class="review-content">
+                    I am placeholder review, I am reviewing the placeholder
+                    and the place is very holding.
+
+                </div>
+                <a class="reply-btn" onClick="reply()"><img src="images/reply.png"/>Reply</a>
+                
+                </div>
+            </div>
+
+            <div class="review-replies review">
+                <div class="line-r"></div>
+                <img src="images/PLACEHOLDER.png" class="pfp"/>
+                <div class="r-box reply-box">
+                <div class="user">Avery Angel <div class="dot"></div>
+                <span class="date">Apr 01 2012</span></div>
+                <div class="review-content">
+                    I am placeholder review, I am reviewing the placeholder
+                    and the place is very holding.
+
+                </div>
+                <a class="reply-btn" onClick="reply()"><img src="images/reply.png"/>Reply</a>
+
+   
+
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    
+        <!-- replying function -->
+
+    <div class="reply-function" id="reply-function">
+        <h3 class="rep-to">reply:</h3>
+        <textarea class="reply-text" placeholder="add a reply" cols="150" rows="5"></textarea>
+        <button class="rep btn">reply</button>
+        <button class="cancel btn" onClick="closeReply()">cancel</button>
+        </div>
+
+    <div class="wr-container" id="wr-container">
+    <div class="write-review">
+    <img src="images/PLACEHOLDER.png" class="pfp self" />
+        <textarea class="form-control" placeholder="add a review" cols="150" rows="5"></textarea>   
+    </div>
+    <button class="post btn">post</button>
+        <button class="cancel btn">cancel</button>
+    </div>
+    </div>
+
+
+    <!-- -->
+
+    <section id="media-wrappper">
+        <h2 class="media-title">MEDIA</h2>
+        <div class="showcase">
+
+        <div class="row row1">
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+        </div>
+        <div class="row row2">
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+        </div>
+        <div class="row row3">
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+            <div class="img-media"><img src="images/PLACEHOLDER.png" /></div>
+        </div>
+
+        </div>
+
+    </section>
+        
+
+
+
+
+        
+    </div>
+    <!-- scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/eab9b1f412.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+            
+
+    <script  src="javascript/function.js"></script> <!-- calendar -->
+    <script>
+        //reply function
+        function reply(){
+        document.getElementById("reply-function").style.display = "inline-block";
+        document.getElementById("wr-container").style.display = "none";
+            }
+
+        function closeReply(){
+            document.getElementById("reply-function").style.display = "none";
+            document.getElementById("wr-container").style.display = "block";
+        }
+
+        //carousel
+        $('.row').slick({
+            prevArrow: false,
+            nextArrow: false,
+            draggable: true,
+            variableWidth: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+         });
+         $('.scroller-area').slick({
+            prevArrow: false,
+            nextArrow: false,
+            draggable: true,
+            adaptiveHeight: true,
+            variableWidth: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: false,
+            autoplaySpeed: 2000,
+
+         });
+         
+        
+
+        
+    
+
+
+    </script> 
+ 
+    
+    </body>
+</html>
