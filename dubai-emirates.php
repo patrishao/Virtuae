516619a6303d81a92d46c92cedbf9fc7b8cde232
Ruth Amurao <72446116+amuraoo@users.noreply.github.com>
2021-05-19 20:53:50 +0400

    dubai page

--- dubai-emirates.php
+++ dubai-emirates.php
@@ -5,101 +5,32 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Dubai</title>
 	<link rel="stylesheet" type="text/css" href="css/dubai-emirates.css">
-    <!-- Font Awesome / link for icons -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 
 </head>
 <body>
     <div class="navbar">
-        <?php include 'includes/headers.php';?> 
+        <?php include 'includes/headers.php';?>
 
-    <!-- wraps whole content -->
-    <div class="wrapper">
-        <div class="container">
-            <div id="title">
-                <h1>DUBAI</h1>
+        <div class="wrapper">
+            <div class="container">
+                <div id="title">
+                    <h1>DUBAI</h1>
+                </div>
+
+                <div class="categories">
+                    <button>EVENTS</button>
+                    <button>DINE</button>
+                    <button>ACTIVTIES</button>
+                    <button>SIGHTS TO SEE</button>
+                    <button>DISCOVER</button>
+                    <button>MOSQUES</button>
+                </div>
             </div>
-
-            <div class="categories">
-                <button>EVENTS</button>
-                <button>DINE</button>
-                <button>ACTIVTIES</button>
-                <button>SIGHTS TO SEE</button>
-                <button>DISCOVER</button>
-                <button>MOSQUES</button>
-            </div>
-
         </div>
     </div>
-    </div> 
 
-   <div class="categ">
-        <div class="post-categ">
-        <h1>MOSQUES IN DUBAI</h1>
-
-        <!-- controls -->
-        <i class="fas fa-chevron-left prev-categ"></i>
-        <i class="fas fa-chevron-right next-categ"></i>
-
-		<section>
-			<div class="slider">
-                <a href="#">
-				<picture>
-					<img src="images/emirates/ad_bg.jpg" alt="">
-				</picture>
-				<div class="detail">
-                    <h3>Dubai Marina</h3>
-                    <h4>Dubai Marina</h4>
-                </div></a>
-			</div>
-            <div class="slider">
-                <a href="#">
-				<picture>
-					<img src="images/marina.jpg" alt="">
-				</picture>
-				<div class="detail">
-                    <h3>Dubai Marina</h3>
-                    <h4>Dubai Marina</h4>
-                </div></a>
-			</div>
-            <div class="slider">
-                <a href="#">
-				<picture>
-					<img src="images/marina.jpg" alt="">
-				</picture>
-				<div class="detail">
-                    <h3>Dubai Marina</h3>
-                    <h4>Dubai Marina</h4>
-                </div></a>
-			</div>
-            <div class="slider">
-                <a href="#">
-				<picture>
-					<img src="images/marina.jpg" alt="">
-				</picture>
-				<div class="detail">
-                    <h3>Dubai Marina</h3>
-                    <h4>Dubai Marina</h4>
-                </div></a>
-			</div>
-            <div class="slider">
-                <a href="#">
-				<picture>
-					<img src="images/emirates/ad_bg.jpg" alt="">
-				</picture>
-				<div class="detail">
-                    <h3>Dubai Marina</h3>
-                    <h4>Dubai Marina</h4>
-                </div></a>
-			</div>
-		</section>
-       
-    </div> 
-
-
-
-
-    <script type="text/javascript" src="javascript/dub-em.js"></script>
+  
+<script type="text/javascript" src="javascript/dub-em.js"></script>
 </body>
 </html>
 
--- javascript/dub-em.js
+++ javascript/dub-em.js
@@ -27,3 +27,28 @@
 	}
 	span[1].onclick = ()=>{right_mover();}
 	span[0].onclick = ()=>{left_mover();}
+
+	
+// Tab views js code
+function openTab(event, tabPages) {
+	var i, x, tablinks;
+  
+	x = document.getElementsByClassName("acc-tabs");
+	for (i = 0; i < x.length; i++) {
+	  x[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < x.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+	}
+	document.getElementById(tabPages).style.display = "block";
+	evt.currentTarget.className += " w3-red";
+  }
+  
+  // Show container on click 
+  function toggleShow(){
+	  var x = document.getElementById("categ");
+	  if (x.style.display === "none") {
+		x.style.display = "block";
+	  } 
+  }
