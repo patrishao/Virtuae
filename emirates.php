b44f77ca41b419be71b9ed2648a110b2630eb95c
Ruth Amurao <72446116+amuraoo@users.noreply.github.com>
2021-05-24 13:37:53 +0400

    changed file name

--- emirates.php
+++ emirates.php
@@ -5,34 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emirates</title>
-    <!-- links -->
-    <link rel="stylesheet" type="text/css" href="css/emirates.css">
-    <!-- link for transition
+    <link rel="stylesheet" href="css/emirates.css">
+     <!-- link for transition
     <link rel="stylesheet" type="text/css" href="css/trans-page.css"> -->
-    <!-- Font Awesome / link for icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-
 </head>
 <body>
-    <!-- navigation bar -->
-    <div class="navbar">
-        <?php include 'includes/headers.php';?>
-        
-        <!-- wraps whole page -->
-        <div class="wrapper">
-        <div class="post-slider">
-            <h1>DUBAI</h1>
-            <a href="dubai.php"><button>Explore</button></a>
+    <div id="navbar">
+        <?php include 'includes/headers.php';?> 
+    </div>
 
-            <!-- left and right buttons to scroll -->
+    <!-- wraps all content -->
+    <div class="wrapper">
+        <div class="container">
+
+            <!-- title and button -->
+            <h1 class="title">DUBAI</h1>
+            <a href="#"><button>Explore</button></a>
+
+            
+            <!--  controls
             <i class="fas fa-chevron-left prev"></i>
-            <i class="fas fa-chevron-right next"></i>
-          
+            <i class="fas fa-chevron-right next"></i>-->
 
-            <!-- wraps content of page -->
-            <div class="post-wrapper">
+            <!-- contains slider images -->
+            <div class="slider">
 
-                <!-- container for images -->
+                <!-- slider image container -->
                 <div class="box">
                         <img src="images/emirates/fuj_bg.jpg" alt="" class="sliderImg">
 
@@ -42,59 +41,59 @@
                         </div>
                 </div>
                 <div class="box">
-                    <img src="images/emirates/ad_bg.jpg" alt="" class="sliderImg">
+                        <img src="images/emirates/ad_bg.jpg" alt="" class="sliderImg">
 
+                        <!-- button -->
                         <div class="hoverButton">
                         <a href="#" class="text">Abu Dhabi</a>
                         </div>
                 </div>
                 <div class="box">
-                    <img src="images/emirates/shj_bg.jpeg" alt="" class="sliderImg">
+                        <img src="images/emirates/shj_bg.jpeg" alt="" class="sliderImg">
 
+                        <!-- button -->
                         <div class="hoverButton">
                         <a href="#" class="text">Sharjah</a>
                         </div>
                 </div>
                 <div class="box">
-                    <img src="images/emirates/uaq_bg.jpg" alt="" class="sliderImg">
+                        <img src="images/emirates/uaq_bg.jpg" alt="" class="sliderImg">
 
+                        <!-- button -->
                         <div class="hoverButton">
                         <a href="#" class="text">Umm Al Quwain</a>
                         </div>
                 </div>
                 <div class="box">
-                    <img src="images/emirates/rak_bg (2).jpg" alt="" class="sliderImg">
+                        <img src="images/emirates/rak_bg (2).jpg" alt="" class="sliderImg">
 
+                        <!-- button -->
                         <div class="hoverButton">
                         <a href="#" class="text">Ras Al Khaima</a>
                         </div>
                 </div>
                 <div class="box">
-                    <img src="images/emirates/ajm_bg.jpg" alt="" class="sliderImg">
+                        <img src="images/emirates/ajm_bg.jpg" alt="" class="sliderImg">
 
+                        <!-- button -->
                         <div class="hoverButton">
                         <a href="#" class="text">Ajman</a>
                         </div>
                 </div>
             </div>
+            
         </div>
-        </div>
-        
     </div>
 
 
 
 
 
-    <!-- JsQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <!-- Slick Carousel -->
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
-    <!-- script link -->
-    <script  type="text/javascript" src="javascript/emirates.js">
-    </script>
+    <script src="javascript/emirates.js"></script>
 
 </body>
 </html>
