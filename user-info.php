5e50dc1a6e168dd3038129c52d6d5807feb7653a
alysxandra <71811550+alysxandra@users.noreply.github.com>
2021-05-29 03:17:06 +0400

    user info wip..

--- user-info.php
+++ user-info.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+    <title>Edit Profile</title>
     <link rel="stylesheet" type="text/css" href="css/user-info.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
@@ -43,7 +43,7 @@
         <a class="acc-s" href="account-settings.php">Account settings</a>
         
         <div class="forms">
-            <p class="name-label">First Nameㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤㅤㅤ Last Name</p>
+            <p class="name-label">First Nameㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤㅤㅤ ㅤㅤㅤㅤㅤㅤㅤLast Name</p>
             <input class="full-name" type="text" placeholder="First Name" /> 
             <input class="last-name" type="text" placeholder="Last Name" />
             <div class="forms2">
