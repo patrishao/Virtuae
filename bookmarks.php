131a308442eaa012dbdf681a0ed2124956ee2fca
saraicarreon <71811580+saraicarreon@users.noreply.github.com>
2021-05-30 15:17:48 +0400

    bookmarks

---
+++ bookmarks.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bookmarks</title>
+	<link rel="stylesheet" type="text/css" href="css/bookmarks.css">
+    
+</head>
+<body>
+    <div id="navbar"> 
+        <?php include 'includes/headers-edit.php';?>
+    </div>
+    
+    <div class="text">Bookmarks</div>
+    
+    <!--classes for 1st row of places -->
+    <div class="placeholder1">
+         <div class="place1"> 
+         <img class="bookmark" src="images/bookmarks-images/plc1.png">
+         <div class="placedesc">
+                <h1> Jebel Jais </h1>
+                <h2> Ras Al Khaimah </h2>
+              </div>
+         </div>
+         <div class="place2">
+         <img class="bookmark" src="images/bookmarks-images/plc2.png">
+         </div>
+         <div class="place3">
+         <img class="bookmark" src="images/bookmarks-images/plc3.png">
+         </div>
+    </div>
+
+    <!--classes for 2nd row of places -->
+    <div class="placeholder2">
+         <div class="place4">
+         <img class="bookmark" src="images/bookmarks-images/plc4.png"> 
+         </div>
+         <div class="place5">
+         <img class="bookmark" src="images/bookmarks-images/plc5.png">
+         </div>
+         <div class="place6">
+         <img class="bookmark" src="images/bookmarks-images/plc6.png">
+         </div>
+    </div>
+
+    <!--classes for 3rd row of places -->
+    <div class="placeholder3">
+         <div class="place7">
+         <img class="bookmark" src="images/bookmarks-images/plc7.png"> 
+         </div>
+         <div class="place8">
+         <img class="bookmark" src="images/bookmarks-images/plc8.png">
+         </div>
+         <div class="place9">
+         <img class="bookmark" src="images/bookmarks-images/plc9.png">
+         </div>
+    </div>
+
+    </body>
+</html>
